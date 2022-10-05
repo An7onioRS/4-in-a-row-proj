61850a37bf45ab2b6d9703f62a8cb06b4050f19f
Antonio RS <antoniosimeonov7@gmail.com>
2022-10-05 19:00:22 +0300

    Fix factory functions and add a method to create tokens

--- js/app.js
+++ js/app.js
@@ -2,22 +2,42 @@ const game = () => {
 
 }
 
-const player = (name, id, active = false, color) => {
-    name,
-    id,
-    active,
-    color
-    tokens = [] // a method will be written later that will be creating the tokens
+const createPlayer = (name, id, active = false, color) => {
+    return {
+        name,
+        id,
+        active,
+        color,
+        tokens:  createTokens(21), // a method will be written later that will be creating the tokens
+        
+        /**
+         * Creates token objects for player
+         * @param {integer} num - Number of token objects to be created
+         */
+
+        createTokens(number) {
+            const tokens = []
+            for (let i = 0; i < number; i++) {
+                tokens.push(createToken(this, i))
+            }
+
+            return tokens
+        }
+    }
 }
 
-const board = () => {
+const createBoard = () => {
 
 }
 
-const token = () => {
-
+const createToken = (owner, index) => {
+    return {
+        owner, // allows access to the Player object (id and color) that owns the token  
+        id: `token-${index}-${owner.id}`, // each token will be creating using a for loop, so we could use the index of the loop
+        dropped: false 
+    }
 }
 
-const space = () => {
+const createSpace = () => {
 
 }
